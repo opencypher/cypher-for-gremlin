369a6bea4e500b7c8a32d28414534aa690907b9b
Dwitry <dwitry@users.noreply.github.com>
2018-02-20 15:16:39 +0200

    Run only translation tests in `testGremlinGroovyTranslation` task

--- testware/integration-tests/src/test/java/org/opencypher/gremlin/translation/VariableLengthPathTest.java
+++ testware/integration-tests/src/test/java/org/opencypher/gremlin/translation/VariableLengthPathTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.opencypher.gremlin;
+package org.opencypher.gremlin.translation;
 
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
