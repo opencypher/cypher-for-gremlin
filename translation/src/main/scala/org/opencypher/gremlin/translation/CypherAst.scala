4aae3b8aa73ad24b38333928d178fd3009949d45
Dimitry Solovyov <369495+disolovyov@users.noreply.github.com>
2018-07-26 15:54:00 +0300

    Update FE to 9.0.5 (#147)
    Signed-off-by: Dimitry Solovyov <dimitry@solovyov.lv>

--- translation/src/main/scala/org/opencypher/gremlin/translation/CypherAst.scala
+++ translation/src/main/scala/org/opencypher/gremlin/translation/CypherAst.scala
@@ -218,6 +218,7 @@ object CypherAst {
     val startState = InitialState(preParsedQueryText, Some(offset), EmptyPlannerName)
     val state = CompilationPhases
       .parsing(RewriterStepSequencer.newPlain, literalExtraction = Never)
+      .andThen(isolateAggregation)
       .andThen(SemanticAnalysis(warn = false))
       .andThen(Normalization)
       .transform(startState, EmptyParserContext(preParsedQueryText, Some(offset)))
